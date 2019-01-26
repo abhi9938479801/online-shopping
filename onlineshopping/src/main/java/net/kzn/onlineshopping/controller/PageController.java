66ce19e2633f7f59041d47ec6873b4a8108909a2
abhi9938479801 <Java.abhishek1996@gmail.com>
2019-01-26 17:27:58 +0530

    project success

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -1,18 +1,27 @@
 package net.kzn.onlineshopping.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import net.kzn.onlineshopping.dao.CategoryDAO;
+
+
 
 @Controller
 public class PageController {
+	
+	@Autowired(required=true)
+	private CategoryDAO categoryDAO;
 	
 	@RequestMapping(value= {"/", "/home", "/index"})
 	public ModelAndView index() {
 		ModelAndView mv=new ModelAndView("page");
 		mv.addObject("title","Home");
+		
+		mv.addObject("categories",categoryDAO.list());
 		mv.addObject("userClickHome",true);
 		return mv;
 	}
---
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/dao/CategoryDAO.java
@@ -0,0 +1,11 @@
+package net.kzn.onlineshopping.dao;
+
+import java.util.List;
+
+import net.kzn.onlineshopping.dto.Category;
+
+public interface CategoryDAO {
+	
+	List<Category> list();
+
+}
